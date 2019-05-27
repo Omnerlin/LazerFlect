40c1daae9526fee4b3d7d8dfe00d946bc7d4b257
Alex Booher <alex.booher@yahoo.com>
2019-05-27 16:12:28 -0600

    Initial Project Commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Tooltip("How fast this bullet will travel.")]
+    public float speed = 5;
+
+    [Tooltip("How long in seconds this bullet will last before being automatically destroyed.")]
+    public float lifetime = 8;
+
+    [Tooltip("How many times this bullet will bounce before being destroyed. Use -1 for infinite possible bounces.")]
+    public float numBounces;
+
+    private float lifeCounter = 0;
+    private Vector3 direction;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        lifeCounter += Time.deltaTime;
+        if(lifeCounter >= lifetime)
+        {
+            Die();
+        }
+    }
+
+    public void Fire(Vector3 startPosition, Vector3 direction)
+    {
+        transform.position = startPosition;
+        transform.forward = direction;
+        this.direction = direction;
+        rb.velocity = direction * speed;
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if(collision.gameObject.CompareTag("Reflector"))
+        {
+            if(--numBounces == -1)
+            {
+                Die();
+            }
+
+            direction = Vector3.Reflect(direction, collision.GetContact(0).normal);
+            transform.forward = direction;
+            rb.velocity = direction * speed;
+        }
+        else
+        {
+            Die();
+        }
+    }
+}
---
+++ Assets/Turret.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [Tooltip("The bullet that will be instantiated when firing")]
+    public GameObject bulletPrefab;
+
+    [Tooltip("What will rotate with our mouse.")]
+    public Transform barrel;
+
+    [Tooltip("Where bullets will be ejected from")]
+    public Transform firePoint;
+
+    [Tooltip("How far the preview line will be drawn")]
+    public float rayLength = 7;
+
+    [Tooltip("Whether or not to draw the shot preview line")]
+    public bool showLine = true;
+
+    private LineRenderer lineRenderer;
+    private Camera cam; // Used for finding out how our turret should be rotated
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetPositions(new Vector3[50]);
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(showLine)
+        {
+            DrawPreviewLine();
+        }
+        UpdateBarrelRotation();
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Bullet bullet = Instantiate(bulletPrefab, firePoint.transform.position, Quaternion.LookRotation(firePoint.forward)).GetComponent<Bullet>();
+            bullet.Fire(firePoint.position, firePoint.forward);
+        }
+    }
+
+    private void DrawPreviewLine()
+    {
+        int hitCount = 0;
+        float lengthLeft = rayLength;
+        bool hit = true;
+        Vector3 rayOrigin = firePoint.position;
+        Vector3 rayDirection = firePoint.forward;
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition(0, rayOrigin);
+
+        while(hit)
+        {
+            Ray lineRay = new Ray(rayOrigin, rayDirection);
+            RaycastHit hitinfo;
+            hit = Physics.Raycast(lineRay, out hitinfo, 500);
+            hitinfo.normal = new Vector3(hitinfo.normal.x, 0, hitinfo.normal.z);
+            if (!hit) hitinfo.distance = 500;
+            lineRenderer.positionCount++;
+            lineRenderer.SetPosition(++hitCount, rayOrigin + lineRay.direction * (hitinfo.distance > lengthLeft ? lengthLeft : hitinfo.distance));
+            lengthLeft -= hitinfo.distance;
+            if(lengthLeft <= 0 || !hit || !hitinfo.collider.CompareTag("Reflector"))
+            {
+                hit = false;
+            }
+            else
+            {
+                rayOrigin = hitinfo.point;
+                rayDirection = Vector3.Reflect(rayDirection, hitinfo.normal);
+            }
+        }
+
+        lineRenderer.positionCount = hitCount + 1;
+    }
+
+    // Rotate barrel to face cursor.
+    private void UpdateBarrelRotation()
+    {
+        Vector3 turret2DPos = cam.WorldToScreenPoint(transform.position);
+        Vector3 mousePos = Input.mousePosition;
+
+        Vector3 targetDir = mousePos - turret2DPos;
+        float angle = -Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg + 90;
+
+        barrel.rotation = Quaternion.AngleAxis(angle, transform.up);
+    }
+}
